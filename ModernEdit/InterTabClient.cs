773a98298c95041d5afc767f49c5a3c506836b9c
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-08-29 14:54:50 +0100

    Added InterTabClient

---
+++ ModernEdit/InterTabClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Dragablz;
+
+namespace ModernEdit
+{
+    public class MyInterTabClient : IInterTabClient
+    {
+        public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            var view = new MainWindow();
+            return new NewTabHost<Window>(view, view.TabablzControl);
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            return TabEmptiedResponse.CloseWindowOrLayoutBranch;
+        }
+    }
+}
