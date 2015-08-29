f5559a62b4cb81666d636acb42c6bb0f0d8246e1
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-08-29 20:42:47 +0100

    Added MEFile Class

---
+++ ModernEdit/FileManagement/MEFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernEdit.FileManagement
+{
+    class MEFile
+    {
+        public string Name = "MEFile";
+        public string Extension = ".txt";
+        public string Content = "This is an example MEFile";
+
+        public MEFile (string name, string extension, string content)
+        {
+            Name = name;
+            Extension = extension;
+            Content = content;
+        }
+    }
+}
