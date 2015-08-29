288a41971967e72866da7fae931c6a70bd357a38
Thomas Peregrine <tomfperegrine@googlemail.com>
2015-08-29 23:22:36 +0100

    Added a FileUtils class

---
+++ ModernEdit/FileManagement/FileUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Win32;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernEdit.FileManagement
+{
+    public static class FileUtils
+    {
+        public static string TextFileFilter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+        static MEFile OpenFile()
+        {
+            string content = "";
+            // Create OpenFileDialog 
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = TextFileFilter;
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                content = File.ReadAllText(dlg.FileName);
+                string FileName = Path.GetFileNameWithoutExtension(dlg.SafeFileName);
+                string Extension = Path.GetExtension(dlg.SafeFileName);
+                return new MEFile(FileName, Extension, content, dlg.FileName);
+            }
+            else
+            {
+                return MEFile.ERROR_ME_FILE;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
